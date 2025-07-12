906d829e0b439354c5b5a74f240608911876d7db
Edson Fernando <ed.f3rn@gmail.com>
2025-07-12 20:18:26 -0300

    Add Strategis

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { LocalStrategy } from './strategies/local.strategy';
+import * as dotenv from 'dotenv';
+import { UserModule } from 'src/user/user.module';
+dotenv.config();
 @Module({
+  imports:[
+    PassportModule,
+   JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+    UserModule
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,LocalStrategy,]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+     constructor(
+   
+    private readonly userService: UserService,
+  ) {}
+
+     async validateUser(Email: string, Senha: string): Promise<User> {
+    const user = await this.userService.findByEmail(Email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(Senha, user.Senha);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          Senha: '',
+        };
+      }
+    }
+
+    throw new NotFoundException(
+      'Email address or password provided is incorrect.',
+    );
+  }
+}
